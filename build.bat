0626e71414bd4331fe24f05b5029e9e1ad3d207e
hushendian <1130397059@qq.com>
2017-08-31 16:39:43 +0800

    腾讯压缩混淆工具

---
+++ build.bat
@@ -0,0 +1,2 @@
+Java -jar AndResGuard-cli-1.2.3.jar test.apk -config config.xml -out release -zipalign E:/sdk/sdk/build-tools/24.0.2/zipalign -7zip "E:\7-zip\7z.exe" -signature C:\Users\hushendian\Desktop\hunxiao\test.jks 123456789 123456789 test1
+
